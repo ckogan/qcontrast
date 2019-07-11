f37851ec2d36812f6c838708f886d4fe4681cc11
Clark Kogan <kogan.clark@gmail.com>
2019-07-11 09:59:46 -0700

    added a few contrast functions

--- R/qcontrast.R
+++ R/qcontrast.R
@@ -16,11 +16,39 @@ contrast_template <- function(model) {
 #' @param term_label
 #' @export
 identify_terms <- function(model, terms) {
-  mm <- model.matrix(mod)
+  mm <- model.matrix(model)
   assi <- attr(mm, "assign")
   lab <- attr(terms(mod), "term.labels")
   num <- which(lab %in% terms)
   as.integer(assi %in% num)
+}
+
+#' Create matrix identifying term levels
+#'
+#' @export
+identify_levels <- function(model, terms) {
+  mm <- model.matrix(model)
+  assi <- attr(mm, "assign")
+  lab <- attr(terms(model), "term.labels")
+  num <- which(lab %in% terms)
+  ix <- which(assi %in% num)
+  mat <- matrix(0, nrow = length(ix) + 1L, ncol = length(assi))
+  for(i in seq_along(ix)) {
+    mat[i+1L, ix[i]] <- 1
+  }
+  mat
+}
+
+#' Create matrix of ordered effects for factor
+#'
+#' @export
+ordered_effects <- function(model, terms) {
+  mat <- identify_levels(model, terms)
+  eff <- matrix(0, nrow = nrow(mat)-1L, ncol = ncol(mat))
+  for(i in seq_len(nrow(eff))) {
+    eff[i,] <- mat[i+1,] - mat[i,]
+  }
+  eff
 }
 
 #' Create vector for averaging term coefficients
